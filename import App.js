30a59659c1a898b695206178317cfcce6b17784c
sugu <151340260+sugu07-M@users.noreply.github.com>
2025-01-23 10:35:03 +0530

    Update import App.js

--- import App.js
+++ import App.js
@@ -5,27 +5,38 @@ import ButtonAppBar from './components/sample.js';
 import Home1 from './components/home1.js';
 import Home2 from './components/home2.js';
 import Home3 from './components/home3.js';
+import { Pro ,Bulid} from './components/exam.js'
 
 
 function App() {
   return (
+   
     <Router>
     <div>
 
-      {/* <ButtonAppBar/> */}
-
-      
+   
       <Routes>
         <Route path="/" element={<ButtonAppBar />} />
         <Route path="/Home1" element={<Home1 />} />
         <Route path="/Home2" element={<Home2 />} />
         <Route path="/Home3" element={<Home3 />}/>
       </Routes>
-    
+   
     </div>
+   <section>
+    <h4>welcome photo</h4>
+    <Pro/>
+    <Bulid/>
+   </section>
    </Router>
+     
+
+
+
   );
 }
 
 export default App;
+
+ 
 
